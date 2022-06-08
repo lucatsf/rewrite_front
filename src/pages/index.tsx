590a7274e857b3b56877e876b7a599a9d00f2c23
Lucas Torres <lucastorresfellicio@gmail.com>
2022-06-08 19:23:06 -0300

    feat: create project

---
+++ src/pages/index.tsx
@@ -0,0 +1,85 @@
+import { Button, Flex, Stack, Textarea, Text, Box } from '@chakra-ui/react';
+import { useEffect, useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { api } from '../services/api';
+
+export default function Home() {
+
+  const { register, handleSubmit } = useForm();
+  const getTextareaRef = useRef<HTMLTextAreaElement>(null)
+  const [ showNewText, setShowNewText ] = useState(false)
+  const [ newText, setNewText ] = useState({})
+
+  function handleTextarea() {
+    if (getTextareaRef.current?.value) {
+      api.post('rewrite', {
+        text: getTextareaRef.current?.value
+      })
+      .then(({ data }) => {
+        if (data.newText) {
+          setShowNewText(true)
+          setNewText(data.newText)
+        }
+      })
+    }
+  }
+
+  return (
+    <>
+        <Text
+          fontSize='4xl'
+          align="center"
+          pb={10}
+        >
+          Digite seu texto
+        </Text>
+
+        <Flex
+          w="100%"
+          h="100%"
+          align="center"
+          justify="center"
+        >
+          <Flex
+            as="form"
+            w="100%"
+            maxWidth={860}
+            bg="gray.800"
+            p="8"
+            borderRadius={8}
+            flexDir="column"
+            onSubmit={handleSubmit(handleTextarea)}
+          >
+            <Stack spacing={10}>
+              <Textarea
+                h={460}
+                resize="none"
+                ref={ getTextareaRef }
+              />
+
+              <Button
+                mt={6}
+                colorScheme="blue"
+                type="submit"
+              >
+                Reescrever texto
+              </Button>
+
+              { showNewText ? (
+                  <>
+                    <Text pt={10}>
+                      { newText.text[0] }
+                    </Text>
+
+                    <Box>
+                      Total de caracteres: { newText.textLength } | Total de palavras: { newText.textWords }
+                    </Box>
+                  </>
+                ) : (<></>)
+              }
+            </Stack>
+          </Flex>
+        </Flex>
+    </>
+  )
+}
