adb686ddb28222c321a4c113748ca14c5371e175
Lucas Torres <lucastorresfellicio@gmail.com>
2022-06-08 21:40:28 -0300

    feat: create environment Variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 
 export const api = axios.create({
-  baseURL: 'https://node-rewrite-api.herokuapp.com/api/',
+  baseURL: process.env.REACT_APP_API_URL,
 })
